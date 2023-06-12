8d128bd421b5702d543e3e11d0706e87a4909132
Vitor Rangel <rangel.final@gmail.com>
2023-06-12 10:34:22 -0300

    Update slug generator to account for non-latin languages

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,7 +1,9 @@
 export const generateSlug = (text: string) =>
     text
+        .normalize("NFD") // Normalize Unicode characters to separate diacritics
+        .replace(/[\u0300-\u036f]/g, "") // Remove diacritical marks
         .toLowerCase() // Convert to lowercase
-        .replace(/[^\w\s-]/g, "") // Remove special characters
+        .replace(/[^\p{L}\p{N}\s-]/gu, "") // Remove non-letter, non-number, non-space, non-dash characters
         .replace(/\s+/g, "-") // Replace spaces with dashes
         .substring(0, 200) // Limit to 200 chars
         .trim(); // Remove leading/trailing spaces
