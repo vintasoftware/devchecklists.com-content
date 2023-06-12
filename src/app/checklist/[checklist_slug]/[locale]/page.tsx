77ce0223b05f4007b52ae9c788ab55b4076b9149
Vitor Rangel <rangel.final@gmail.com>
2023-06-12 14:32:28 -0300

    Add basic JSON-LD

--- src/app/checklist/[checklist_slug]/[locale]/page.tsx
+++ src/app/checklist/[checklist_slug]/[locale]/page.tsx
@@ -1,11 +1,15 @@
 import { Avatar } from "@/components/avatar";
-import { ChecklistService } from "@/services/checklist";
+import {
+    Checklist as IChecklist,
+    ChecklistService,
+} from "@/services/checklist";
 import { Tags } from "@/components/tag";
 import { CircleStackIcon } from "@heroicons/react/24/solid";
 import { Checklist } from "@/components/checklist";
 import { LocalesDropdown } from "@/components/localeDropdown";
 import Link from "next/link";
 import { Metadata } from "next";
+import Script from "next/script";
 
 interface ChecklistParam {
     checklist_slug: string;
@@ -40,6 +44,20 @@ export async function generateMetadata({
     };
 }
 
+// Used to provide rich results for search engines
+// https://developers.google.com/search/docs/appearance/structured-data/how-to
+export function generateJSONLD(checklist: IChecklist) {
+    return {
+        __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "HowTo",
+            name: checklist.frontmatter.title ?? checklist.slug,
+            // TODO: Extract checkitems and add them to JSON-LD
+            step: [],
+        }),
+    };
+}
+
 const ChecklistPage = ({
     params: { checklist_slug: slug, locale },
 }: {
@@ -61,49 +79,59 @@ const ChecklistPage = ({
     } = checklist;
 
     return (
-        <div className="flex flex-col container mx-auto">
-            <div className="flex justify-between">
-                <div className="flex flex-col gap-4 my-10">
-                    <div className="flex justify-between">
-                        {title && (
-                            <h2 className="text-4xl font-mono">{title}</h2>
-                        )}
-                    </div>
-                    <div className="checklist-author">
-                        <Link
-                            href={
-                                author_username
-                                    ? `https://github.com/${author_username}/`
-                                    : "#"
-                            }
-                            target="_blank"
-                            className="flex gap-2 text-blue items-center"
-                        >
-                            {author_username && (
-                                <Avatar username={author_username} size={35} />
+        <>
+            <Script
+                id="ld-json"
+                type="application/ld+json"
+                dangerouslySetInnerHTML={generateJSONLD(checklist)}
+            />
+            <div className="flex flex-col container mx-auto">
+                <div className="flex justify-between">
+                    <div className="flex flex-col gap-4 my-10">
+                        <div className="flex justify-between">
+                            {title && (
+                                <h2 className="text-4xl font-mono">{title}</h2>
                             )}
-                            {author_name && <h5>{author_name}</h5>}
-                        </Link>
+                        </div>
+                        <div className="checklist-author">
+                            <Link
+                                href={
+                                    author_username
+                                        ? `https://github.com/${author_username}/`
+                                        : "#"
+                                }
+                                target="_blank"
+                                className="flex gap-2 text-blue items-center"
+                            >
+                                {author_username && (
+                                    <Avatar
+                                        username={author_username}
+                                        size={35}
+                                    />
+                                )}
+                                {author_name && <h5>{author_name}</h5>}
+                            </Link>
+                        </div>
+                        <h5 className="text-light-gray">{description}</h5>
+                        <Tags tags={tags} />
+                        <div className="flex text-blue gap-2 items-center">
+                            <CircleStackIcon title="Storage" className="h-4" />
+                            <h5>Checks are saved to your local storage</h5>
+                        </div>
                     </div>
-                    <h5 className="text-light-gray">{description}</h5>
-                    <Tags tags={tags} />
-                    <div className="flex text-blue gap-2 items-center">
-                        <CircleStackIcon title="Storage" className="h-4" />
-                        <h5>Checks are saved to your local storage</h5>
+                    <div className="mt-10">
+                        <LocalesDropdown
+                            availableLocales={availableLocales}
+                            hrefPrefix={`/checklist/${slug}/`}
+                        />
                     </div>
                 </div>
-                <div className="mt-10">
-                    <LocalesDropdown
-                        availableLocales={availableLocales}
-                        hrefPrefix={`/checklist/${slug}/`}
-                    />
-                </div>
-            </div>
 
-            <article className="prose prose-invert prose-ul:list-none max-w-3xl px-10 py-4 bg-dark-gray rounded-md">
-                <Checklist checklistHTML={checklist.contentHtml} />
-            </article>
-        </div>
+                <article className="prose prose-invert prose-ul:list-none max-w-3xl px-10 py-4 bg-dark-gray rounded-md">
+                    <Checklist checklistHTML={checklist.contentHtml} />
+                </article>
+            </div>
+        </>
     );
 };
 
