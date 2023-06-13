1b92bc63d6e3bc1edd363b7f1e72dc9a780d734f
Vitor Rangel <rangel.final@gmail.com>
2023-06-13 10:50:42 -0300

    Better cache for github avatars

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export", // Enables static site generation
-    images: { unoptimized: true }, // Image Optimization API is not compatible with static site generation
+    images: {
+        unoptimized: true, // Image Optimization API is not compatible with static site generation
+        remotePatterns: [{ protocol: "https", hostname: "github.com" }], // Github Avatars
+    },
     reactStrictMode: true, // Enables React Strict Mode
 };
 
