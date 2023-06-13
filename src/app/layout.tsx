d99b0908d4e08ff0194a6fdd69640520a1c6bd4e
Vitor Rangel <rangel.final@gmail.com>
2023-06-13 12:47:21 -0300

    Update GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Header } from "../components/header";
 import "./globals.css";
 import { Roboto_Flex, Roboto_Mono } from "next/font/google";
 import { Metadata } from "next";
+import { BodyGTM, HeadGTM } from "@/components/gtm";
 
 // Loads fonts: https://nextjs.org/docs/pages/building-your-application/optimizing/fonts
 export const robotoFlex = Roboto_Flex({ subsets: ["latin"], display: "swap" });
@@ -12,22 +13,9 @@ export const robotoMono = Roboto_Mono({ subsets: ["latin"], display: "swap" });
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <Script id="google-tag-manager" strategy="afterInteractive">
-                {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                '../www.googletagmanager.com/gtm5445.html?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-WFBZNP9');`}
-            </Script>
+            <HeadGTM />
             <body className="bg-light-black">
-                <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-WFBZNP9"
-                        height="0"
-                        width="0"
-                        style={{ display: "none", visibility: "hidden" }}
-                    ></iframe>
-                </noscript>
+                <BodyGTM />
                 <Header />
                 <div className="mx-auto min-h-screen max-w-5xl">{children}</div>
                 <Footer />
