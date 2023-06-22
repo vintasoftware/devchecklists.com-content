e98c12c243dff472577286ac5c048bd0cc18256b
Vitor Rangel <rangel.final@gmail.com>
2023-06-22 10:54:11 -0300

    Add more tests

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -42,3 +42,80 @@ test("Navigate around the site", async ({ page }) => {
         .click();
     await expect(page).toHaveURL("/checklist/design-specs-interaction/en");
 });
+
+test("Click on an author link and go to github", async ({ page, context }) => {
+    // Go to Home
+    await page.goto("/");
+
+    // Check if link opened on a new tab
+    // https://playwright.dev/docs/pages#handling-new-pages
+    const pagePromise = context.waitForEvent("page");
+
+    // Click on fjsj name
+    await page
+        .getByRole("link", { name: "fjsj Flávio Juvenal da Silva Junior" })
+        .first()
+        .click();
+
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
+
+    expect(newPage).toBeDefined();
+    await expect(newPage).toHaveURL("https://github.com/fjsj/");
+});
+
+test("Authors without author_username should not be links", async ({
+    page,
+}) => {
+    // Go to Home
+    await page.goto("/");
+
+    // Aline does not have a github username, so there should be no link with her name
+    expect(
+        await page.getByRole("link", { name: "Aline Silveira" }).count()
+    ).toBe(0);
+});
+
+test("Click on Vinta's logo and go to Vinta homepage", async ({
+    page,
+    context,
+}) => {
+    // Go to Home
+    await page.goto("/");
+
+    // Check if link opened on a new tab
+    // https://playwright.dev/docs/pages#handling-new-pages
+    const pagePromise = context.waitForEvent("page");
+
+    // Click on Vinta's logo on the footer
+    await page.getByRole("link", { name: "Vinta Software Logo" }).click();
+
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
+
+    expect(newPage).toBeDefined();
+    await expect(newPage).toHaveURL("https://www.vintasoftware.com/");
+});
+
+test("Click on Collaborate button and go to collaborate.md on github", async ({
+    page,
+    context,
+}) => {
+    // Go to Home
+    await page.goto("/");
+
+    // Check if link opened on a new tab
+    // https://playwright.dev/docs/pages#handling-new-pages
+    const pagePromise = context.waitForEvent("page");
+
+    // Click on Collaborate button on the header
+    await page.getByRole("link", { name: "+ Collaborate" }).click();
+
+    const newPage = await pagePromise;
+    await newPage.waitForLoadState();
+
+    expect(newPage).toBeDefined();
+    await expect(newPage).toHaveURL(
+        "https://github.com/vintasoftware/devchecklists.com-content/blob/main/COLLABORATE.md"
+    );
+});
